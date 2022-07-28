f33500dc1564f2fd28d67f9e1639bee402d2e31f
Aolin <30463932+Aolin-MIR@users.noreply.github.com>
2022-07-28 18:33:00 +0800

    Create double_trans.py

---
+++ double_trans.py
@@ -0,0 +1,549 @@
+import torch
+import torch.nn as nn
+from src.models.sequence.ss.standalone.s4 import LinearActivation, S4
+from einops import rearrange
+class DownPool(nn.Module):
+    def __init__(self, d_input, expand, pool):
+        super().__init__()
+        self.d_output = d_input * expand
+        self.pool = pool
+
+        self.linear = LinearActivation(
+            d_input * pool,
+            self.d_output,
+            transposed=True,
+            weight_norm=True,
+        )
+
+    def forward(self, x):
+        x = rearrange(x, '... h (l s) -> ... (h s) l', s=self.pool)
+        x = self.linear(x)
+        return x, None
+
+    def step(self, x, state, **kwargs):
+        """
+        x: (..., H)
+        """
+
+        if x is None: return None, state
+        state.append(x)
+        if len(state) == self.pool:
+            x = rearrange(torch.stack(state, dim=-1), '... h s -> ... (h s)')
+            x = x.unsqueeze(-1)
+            x = self.linear(x)
+            x = x.squeeze(-1)
+            return x, []
+        else:
+            return None, state
+
+    def default_state(self, *args, **kwargs):
+        return []
+
+
+class UpPool(nn.Module):
+    def __init__(self, d_input, expand, pool):
+        super().__init__()
+        self.d_output = d_input // expand
+        self.pool = pool
+
+        self.linear = LinearActivation(
+            d_input,
+            self.d_output * pool,
+            transposed=True,
+            weight_norm=True,
+        )
+
+    def forward(self, x, skip=None):
+        x = self.linear(x)
+        
+        x = F.pad(x[..., :-1], (1, 0)) # Shift to ensure causality
+        x = rearrange(x, '... (h s) l -> ... h (l s)', s=self.pool)
+    
+        if skip is not None:
+            x = x + skip
+        return x, None
+
+    def step(self, x, state, **kwargs):
+        """
+        x: (..., H)
+        """
+        assert len(state) > 0
+        y, state = state[0], state[1:]
+        if len(state) == 0:
+            assert x is not None
+            x = x.unsqueeze(-1)
+            x = self.linear(x)
+            x = x.squeeze(-1)
+            x = rearrange(x, '... (h s) -> ... h s', s=self.pool)
+            state = list(torch.unbind(x, dim=-1))
+        else: assert x is None
+        return y, state
+
+    def default_state(self, *batch_shape, device=None):
+        state = torch.zeros(batch_shape + (self.d_output, self.pool), device=device) # (batch, h, s)
+        state = list(torch.unbind(state, dim=-1)) # List of (..., H)
+        return state
+
+class FFBlock(nn.Module):
+
+    def __init__(self, d_model, expand=2, dropout=0.0):
+        """
+        Feed-forward block.
+
+        Args:
+            d_model: dimension of input
+            expand: expansion factor for inverted bottleneck
+            dropout: dropout rate
+        """
+        super().__init__()
+
+        input_linear = LinearActivation(
+            d_model, 
+            d_model * expand,
+            transposed=True,
+            activation='gelu',
+            activate=True,
+        )
+        dropout = nn.Dropout2d(dropout) if dropout > 0.0 else nn.Identity()
+        output_linear = LinearActivation(
+            d_model * expand,
+            d_model, 
+            transposed=True,
+            activation=None,
+            activate=False,
+        )
+
+        self.ff = nn.Sequential(
+            input_linear,
+            dropout,
+            output_linear,
+        )
+
+    def forward(self, x):
+        return self.ff(x), None
+
+    def default_state(self, *args, **kwargs):
+        return None
+
+    def step(self, x, state, **kwargs):
+        # expects: (B, D, L)
+        return self.ff(x.unsqueeze(-1)).squeeze(-1), state
+
+
+class ResidualBlock(nn.Module):
+
+    def __init__(
+        self, 
+        d_model, 
+        layer,
+        dropout=0.0,
+    ):
+        """
+        Residual S4 block.
+
+        Args:
+            d_model: dimension of the model
+            bidirectional: use bidirectional S4 layer
+            glu: use gated linear unit in the S4 layer
+            dropout: dropout rate
+        """
+        super().__init__()
+
+        self.layer = layer
+        self.norm = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout2d(dropout) if dropout > 0.0 else nn.Identity()
+
+    def forward(self, x):
+        """
+        Input x is shape (B, d_input, L)
+        """
+        z = x
+        
+        # Prenorm
+        z = self.norm(z.transpose(-1, -2)).transpose(-1, -2)
+        
+        # Apply layer: we ignore the state input and output for training
+        z, _ = self.layer(z)
+
+        # Dropout on the output of the layer
+        z = self.dropout(z)
+
+        # Residual connection
+        x = z + x
+
+        return x, None
+
+    def default_state(self, *args, **kwargs):
+        return self.layer.default_state(*args, **kwargs)
+
+    def step(self, x, state, **kwargs):
+        z = x
+
+        # Prenorm
+        z = self.norm(z)
+
+        # Apply layer
+        z, state = self.layer.step(z, state, **kwargs)
+
+        # Residual connection
+        x = z + x
+
+        return x, state
+class DoubleTrans(nn.Module):
+    def __init__(self,
+        d_model=64, 
+        n_layers=8, 
+        pool=[4, 4], 
+        expand=2, 
+        ff=2, 
+        bidirectional=False,
+        glu=True,
+        unet=False,
+        dropout=0.0,
+    ):
+        super().__init__()
+        self.d_model = H = d_model
+        self.unet = unet
+
+        def s4_block(dim):
+            layer = S4(
+                d_model=dim, 
+                d_state=64,
+                bidirectional=bidirectional,
+                postact='glu' if glu else None,
+                dropout=dropout,
+                transposed=True,
+                # hurwitz=True, # use the Hurwitz parameterization for stability
+                # tie_state=True, # tie SSM parameters across d_state in the S4 layer
+                trainable={
+                    'dt': True,
+                    'A': True,
+                    'P': True,
+                    'B': True,
+                }, # train all internal S4 parameters
+                    
+            )
+            return ResidualBlock(
+                d_model=dim,
+                layer=layer,
+                dropout=dropout,
+            )
+
+        def ff_block(dim):
+            layer = FFBlock(
+                d_model=dim,
+                expand=ff,
+                dropout=dropout,
+            )
+            return ResidualBlock(
+                d_model=dim,
+                layer=layer,
+                dropout=dropout,
+            )
+
+        # Down blocks
+        d_layers = []
+        for p in pool:
+            if unet:
+                # Add blocks in the down layers
+                for _ in range(n_layers):
+                    d_layers.append(s4_block(H))
+                    if ff > 0: d_layers.append(ff_block(H))
+
+            # Add sequence downsampling and feature expanding
+            d_layers.append(DownPool(H, expand, p))
+            H *= expand
+        
+        # Center block
+        c_layers = []
+        for _ in range(n_layers):
+            c_layers.append(s4_block(H))
+            if ff > 0: c_layers.append(ff_block(H))
+        
+        # Up blocks
+        u_layers = []
+        for p in pool[::-1]:
+            block = []
+            H //= expand
+            block.append(UpPool(H * expand, expand, p))
+
+            for _ in range(n_layers):
+                block.append(s4_block(H))
+                if ff > 0: block.append(ff_block(H))
+
+            u_layers.append(nn.ModuleList(block))
+        
+        self.d_layers = nn.ModuleList(d_layers)
+        self.c_layers = nn.ModuleList(c_layers)
+        self.u_layers = nn.ModuleList(u_layers)
+        self.norm = nn.LayerNorm(H)
+
+        assert H == d_model
+
+    def forward(self, x_list, state=None):
+        """
+        input: (batch, length, d_input)
+        output: (batch, length, d_output)
+        """
+        # _x_list=[]
+        # for x in x_list:
+        #     _x_list.append(x.transpose(1, 2))
+
+
+        # # Down blocks
+        # for x in _x_list:
+        #     outputs = []
+        #     outputs.append(x)
+        #     for layer in self.d_layers:
+        #         x, _ = layer(x)
+        #         outputs.append(x)
+
+
+
+        
+        t=[]
+        s=[]
+        for x in x_list:
+            x=x.transpose(1, 2)
+            outputs = []
+            outputs.append(x)
+            # Down blocks
+            for layer in self.d_layers:
+                x, _ = layer(x)
+                outputs.append(x)  
+                    
+            for layer in self.c_layers:
+                x, _ = layer(x)
+            # add a skip connection to the last output of the down block
+            x = x + outputs.pop()
+
+            temp=x.shape[-1]    
+            _t,_s=torch.split(x,int(temp/2),dim=-1)
+            t.append(_t)
+            s.append(_s)
+            #Todo:transcription decoder
+        
+
+
+        # Up blocks
+        for block in self.u_layers:
+            if self.unet:
+                for layer in block:
+                    x, _ = layer(x)
+                    x = x + outputs.pop() # skip connection
+            else:
+                for layer in block:
+                    x, _ = layer(x)
+                    if isinstance(layer, UpPool):
+                        # Before modeling layer in the block
+                        x = x + outputs.pop()
+                        outputs.append(x)
+                x = x + outputs.pop() # add a skip connection from the input of the modeling part of this up block
+
+        # feature projection
+        x = x.transpose(1, 2) # (batch, length, expand)
+        x = self.norm(x)
+
+        return y1,y2,y3,y4,y5,y6,y7,y8, None # required to return a state
+
+
+def train(epoch):
+    global global_step
+
+    print("epoch %3d with lr=%.02e" % (epoch, get_lr()))
+    phase = 'train'
+    writer.add_scalar('%s/learning_rate' % phase,  get_lr(), epoch)
+
+    model.train()  # Set model to training mode
+
+    running_loss = 0.0
+    it = 0
+    correct = 0
+    total = 0
+
+    pbar = tqdm(train_dataloader, unit="audios", unit_scale=train_dataloader.batch_size)
+    for batch in pbar:
+        inputs = batch['input']
+        inputs = torch.unsqueeze(inputs, 1)
+        targets = batch['target']
+
+        inputs = Variable(inputs, requires_grad=True)
+        targets = Variable(targets, requires_grad=False)
+
+        if use_gpu:
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+        # forward/backward
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # statistics
+        it += 1
+        global_step += 1
+        running_loss += loss.item() # loss.data[0]
+        pred = outputs.data.max(1, keepdim=True)[1]
+        correct += pred.eq(targets.data.view_as(pred)).sum()
+        total += targets.size(0)
+
+        writer.add_scalar('%s/loss' % phase, loss.item(), global_step)
+
+        # update the progress bar
+        pbar.set_postfix({
+            'loss': "%.05f" % (running_loss / it),
+            'acc': "%.02f%%" % (100*correct/total)
+        })
+
+    accuracy = correct/total
+    epoch_loss = running_loss / it
+    writer.add_scalar('%s/accuracy' % phase, 100*accuracy, epoch)
+    writer.add_scalar('%s/epoch_loss' % phase, epoch_loss, epoch)
+    
+def criterion(outputs):
+
+def train(epoch):
+    global global_step
+
+    print("epoch %3d with lr=%.02e" % (epoch, get_lr()))
+    phase = 'train'
+    writer.add_scalar('%s/learning_rate' % phase,  get_lr(), epoch)
+
+    model.train()  # Set model to training mode
+
+    running_loss = 0.0
+    it = 0
+    correct = 0
+    total = 0
+
+    pbar = tqdm(train_dataloader, unit="audios", unit_scale=train_dataloader.batch_size)
+    for batch in pbar:
+        inputs = batch['input']
+        inputs = torch.unsqueeze(inputs, 1)
+        targets = batch['target']
+
+        inputs = Variable(inputs, requires_grad=True)
+        targets = Variable(targets, requires_grad=False)
+
+        if use_gpu:
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+        # forward/backward
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # statistics
+        it += 1
+        global_step += 1
+        running_loss += loss.item() # loss.data[0]
+        pred = outputs.data.max(1, keepdim=True)[1]
+        correct += pred.eq(targets.data.view_as(pred)).sum()
+        total += targets.size(0)
+
+        writer.add_scalar('%s/loss' % phase, loss.item(), global_step)
+
+        # update the progress bar
+        pbar.set_postfix({
+            'loss': "%.05f" % (running_loss / it),
+            'acc': "%.02f%%" % (100*correct/total)
+        })
+
+    accuracy = correct/total
+    epoch_loss = running_loss / it
+    writer.add_scalar('%s/accuracy' % phase, 100*accuracy, epoch)
+    writer.add_scalar('%s/epoch_loss' % phase, epoch_loss, epoch)
+
+def valid(epoch):
+    global best_accuracy, best_loss, global_step
+
+    phase = 'valid'
+    model.eval()  # Set model to evaluate mode
+
+    running_loss = 0.0
+    it = 0
+    correct = 0
+    total = 0
+
+    pbar = tqdm(valid_dataloader, unit="audios", unit_scale=valid_dataloader.batch_size)
+    for batch in pbar:
+        inputs = batch['input']
+        inputs = torch.unsqueeze(inputs, 1)
+        targets = batch['target']
+
+        inputs = Variable(inputs, volatile = True)
+        targets = Variable(targets, requires_grad=False)
+
+        if use_gpu:
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+        # forward
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        # statistics
+        it += 1
+        global_step += 1
+        running_loss += loss.item()
+        pred = outputs.data.max(1, keepdim=True)[1]
+        correct += pred.eq(targets.data.view_as(pred)).sum()
+        total += targets.size(0)
+
+        writer.add_scalar('%s/loss' % phase, loss.item(), global_step)
+
+        # update the progress bar
+        pbar.set_postfix({
+            'loss': "%.05f" % (running_loss / it),
+            'acc': "%.02f%%" % (100*correct/total)
+        })
+
+    accuracy = correct/total
+    epoch_loss = running_loss / it
+    writer.add_scalar('%s/accuracy' % phase, 100*accuracy, epoch)
+    writer.add_scalar('%s/epoch_loss' % phase, epoch_loss, epoch)
+
+    checkpoint = {
+        'epoch': epoch,
+        'step': global_step,
+        'state_dict': model.state_dict(),
+        'loss': epoch_loss,
+        'accuracy': accuracy,
+        'optimizer' : optimizer.state_dict(),
+    }
+
+    if accuracy > best_accuracy:
+        best_accuracy = accuracy
+        torch.save(checkpoint, 'checkpoints/best-loss-speech-commands-checkpoint-%s.pth' % full_name)
+        torch.save(model, '%d-%s-best-loss.pth' % (start_timestamp, full_name))
+    if epoch_loss < best_loss:
+        best_loss = epoch_loss
+        torch.save(checkpoint, 'checkpoints/best-acc-speech-commands-checkpoint-%s.pth' % full_name)
+        torch.save(model, '%d-%s-best-acc.pth' % (start_timestamp, full_name))
+
+    torch.save(checkpoint, 'checkpoints/last-speech-commands-checkpoint.pth')
+    del checkpoint  # reduce memory
+
+    return epoch_loss
+
+print("training %s for Google speech commands..." % 'resnext')
+since = time.time()
+for epoch in range(start_epoch, args.max_epochs):
+    if args.lr_scheduler == 'step':
+        lr_scheduler.step()
+
+    train(epoch)
+    epoch_loss = valid(epoch)
+
+    if args.lr_scheduler == 'plateau':
+        lr_scheduler.step(metrics=epoch_loss)
+
+    time_elapsed = time.time() - since
+    time_str = 'total time elapsed: {:.0f}h {:.0f}m {:.0f}s '.format(time_elapsed // 3600, time_elapsed % 3600 // 60, time_elapsed % 60)
+    print("%s, best accuracy: %.02f%%, best loss %f" % (time_str, 100*best_accuracy, best_loss))
+print("finished")
