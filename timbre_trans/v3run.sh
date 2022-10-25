622522da694097d9e968fdaa3ad13d18f2cd64f1
Aolin <30463932+Aolin-MIR@users.noreply.github.com>
2022-10-25 12:59:15 +0800

    Delete v3run.sh

--- timbre_trans/v3run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source ~/.bashrc
-cd /data/state-spaces-main/sashimi/
-python -u fast_thoegaze_v3.py --batch_size=32 --beta=0.7 --gamma=0.3 --train_nums=183347 --valid_nums=32504 >trainlog_$(date +%m%d%H%M)
